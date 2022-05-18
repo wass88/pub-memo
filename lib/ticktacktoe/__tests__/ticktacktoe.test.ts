6584b41540e5313c48bc45a8d863b8284d6ed084
wass80 <wasss80@gmail.com>
2022-05-18 16:47:01 +0900

    wip

--- lib/ticktacktoe/__tests__/ticktacktoe.test.ts
+++ lib/ticktacktoe/__tests__/ticktacktoe.test.ts
@@ -1,18 +1,18 @@
-import {State} from "../ticktacktoe"
+import {Config, State} from "../ticktacktoe"
 
 
 test("init", ()=>{
-    expect(new State(2).rank).toBe(2)
+    expect(new State(new Config(2)).config.rank).toBe(2)
 })
 
 test("board", ()=>{
-    const state = new State(1);
-    const board = state.board();
-    expect(board.board.length).toBe(9);
+    const state = new State(new Config(2));
+    const board = state.board;
+    expect(board.length).toBe(9);
 })
 
 test("all", ()=>{
-    const state = new State(1);
-    const board = state.playable();
+    const state = new State(new Config(2));
+    const playable = state.playable();
     expect(playable.length).toBe(9);
 })
--- lib/ticktacktoe/ticktacktoe.ts
+++ lib/ticktacktoe/ticktacktoe.ts
@@ -1,27 +1,37 @@
+export class Config {
+    rank: number;
+    constructor(rank: number) {
+        this.rank = rank;
+    }
+}
 export type Action = {
     pos: number[];
     first: boolean;
 }
 
 export class State {
-    rank: number;
+    config: Config;
     record: Action[];
     first: boolean;
     board: number[];
     lastMoves: number[];
-    constructor(rank: number) {
-        this.rank = rank;
+    // x -> 9x - 9x+8
+    constructor(config: Config) {
+        this.config = config;
         this.record = [];
         this.first = true;
-        const length = Math.pow(9, rank)
+        const length = Math.pow(9, config.rank)
         this.board = Array(length).fill(0)
     }
     playable(): number[]{
-
+        
     }
     bfs(node: number, r: number): {winner: number, playable: boolean} [] {
         if (this.isLeaf(node)) {
 
         }
+    }
+    isLeaf(node: number) {
+        Math.pow(9, config.rank)
     }
 }
