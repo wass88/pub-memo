b8afd542cbdfb69bca918837af157e0d3c29691b
wass80 <wasss80@gmail.com>
2022-05-19 11:27:48 +0900

    wip

--- lib/ticktacktoe/ticktacktoe.ts
+++ lib/ticktacktoe/ticktacktoe.ts
@@ -1,3 +1,5 @@
+import { rmSync } from "fs";
+
 export class Config {
     rank: number;
     constructor(rank: number) {
@@ -9,13 +11,19 @@ export type Action = {
     first: boolean;
 }
 
+const AtomSize = 9;
 export class State {
     config: Config;
     record: Action[];
     first: boolean;
     board: number[];
     lastMoves: number[];
-    // x -> 9x - 9x+8
+    // x * 9 + 1 ~ x * 9 + 9
+    //0: 0
+    //1: 1     2   ~  9
+    //2: 10-18 19-
+    //3:
+
     constructor(config: Config) {
         this.config = config;
         this.record = [];
@@ -27,11 +35,19 @@ export class State {
         
     }
     bfs(node: number, r: number): {winner: number, playable: boolean} [] {
-        if (this.isLeaf(node)) {
+        if (this.pos(node)[]) {
 
         }
     }
-    isLeaf(node: number) {
-        Math.pow(9, config.rank)
+    isLeaf(node:number): boolean {
+        return pos[pos.length - 1]
+    }
+    pos(node: number) : number[] {
+        const res = [];
+        if (node === 0) {
+            return res.reverse();
+        }
+        res.push((node - 1) % 9);
+        node = (node - 1) / 9;
     }
 }
