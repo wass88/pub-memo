165c1656ce91ceef841e675bef78b179b06e69a3
wass80 <wasss80@gmail.com>
2022-05-21 12:05:39 +0900

    wip

--- lib/ticktacktoe/__tests__/ticktacktoe.test.ts
+++ lib/ticktacktoe/__tests__/ticktacktoe.test.ts
@@ -1,18 +1,30 @@
-import {Config, State} from "../ticktacktoe"
+import { Config, State, Piece } from "../ticktacktoe";
 
+test("init", () => {
+  expect(new State(new Config(2)).config.rank).toBe(2);
+});
 
-test("init", ()=>{
-    expect(new State(new Config(2)).config.rank).toBe(2)
-})
+test("board", () => {
+  const state = new State(new Config(2));
+  const board = state.board;
+  expect(board.length).toBe(81);
+});
 
-test("board", ()=>{
-    const state = new State(new Config(2));
-    const board = state.board;
-    expect(board.length).toBe(9);
-})
-
-test("all", ()=>{
-    const state = new State(new Config(2));
-    const playable = state.playable();
-    expect(playable.length).toBe(9);
-})
+test("bfs", () => {
+  const state = new State(new Config(2));
+  expect(state.boardNumber(9)).toBe(null);
+  expect(state.boardNumber(10)).toBe(0);
+  expect(state.boardNumber(11)).toBe(1);
+  expect(state.boardNumber(90)).toBe(80);
+  const bfs = state.bfs(0);
+  expect(bfs.type).toBe("node");
+  expect(bfs.result).toBe(Piece.Blank);
+  if (bfs.type == "node") {
+    expect(bfs.board[0].type).toBe("node");
+    if (bfs.board[0].type == "node") {
+      expect(bfs.board[0].board[0].type).toBe("leaf");
+    } else {
+      fail("unexpected");
+    }
+  }
+});
--- lib/ticktacktoe/ticktacktoe.ts
+++ lib/ticktacktoe/ticktacktoe.ts
@@ -1,53 +1,119 @@
-import { rmSync } from "fs";
-
 export class Config {
-    rank: number;
-    constructor(rank: number) {
-        this.rank = rank;
-    }
+  rank: number;
+  constructor(rank: number) {
+    this.rank = rank;
+  }
 }
 export type Action = {
-    pos: number[];
-    first: boolean;
-}
+  pos: number[];
+  first: boolean;
+};
 
 const AtomSize = 9;
+export const Piece = {
+  First: 1,
+  Second: 2,
+  Blank: 0,
+  Draw: 3,
+};
+type Piece = typeof Piece[keyof typeof Piece];
+
+const RowOrder = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
+type BfsResult =
+  | {
+      type: "leaf";
+      result: Piece;
+      board_pos: number;
+    }
+  | {
+      type: "node";
+      result: Piece;
+      board: BfsResult[];
+    };
 export class State {
-    config: Config;
-    record: Action[];
-    first: boolean;
-    board: number[];
-    lastMoves: number[];
-    // x * 9 + 1 ~ x * 9 + 9
-    //0: 0
-    //1: 1     2   ~  9
-    //2: 10-18 19-
-    //3:
+  config: Config;
+  record: Action[];
+  first: boolean;
+  board: Piece[];
+  lastMoves: number[];
+  // x * 9 + 1 ~ x * 9 + 9
+  //-: 0
+  //1: 1     2   ~  9
+  //2: 10-18 19-    82-90
+  //3:
 
-    constructor(config: Config) {
-        this.config = config;
-        this.record = [];
-        this.first = true;
-        const length = Math.pow(9, config.rank)
-        this.board = Array(length).fill(0)
-    }
-    playable(): number[]{
-        
-    }
-    bfs(node: number, r: number): {winner: number, playable: boolean} [] {
-        if (this.pos(node)[]) {
-
+  constructor(config: Config) {
+    this.config = config;
+    this.record = [];
+    this.first = true;
+    const length = Math.pow(9, config.rank);
+    this.board = Array(length).fill(0);
+  }
+  bfs(node: number): BfsResult {
+    function winner(atomBoard: Piece[]): Piece {
+      if (atomBoard.length !== AtomSize) {
+        throw new Error(`atomBoard length !== ${AtomSize}. ${atomBoard}`);
+      }
+      RowOrder.forEach((r) => {
+        if (r.every((x) => atomBoard[x] === Piece.First)) {
+          return Piece.First;
         }
-    }
-    isLeaf(node:number): boolean {
-        return pos[pos.length - 1]
-    }
-    pos(node: number) : number[] {
-        const res = [];
-        if (node === 0) {
-            return res.reverse();
+        if (r.every((x) => atomBoard[x] === Piece.Second)) {
+          return Piece.Second;
         }
-        res.push((node - 1) % 9);
-        node = (node - 1) / 9;
+      });
+      if (atomBoard.some((x) => x === Piece.Blank)) {
+        return Piece.Blank;
+      }
+      return Piece.Draw;
     }
+    const boardNumber = this.boardNumber(node);
+    if (boardNumber !== null) {
+      return {
+        type: "leaf",
+        result: this.board[boardNumber],
+        board_pos: boardNumber,
+      };
+    }
+    const child = node * AtomSize + 1;
+    const results = [];
+    for (let st = 0; st < AtomSize; st++) {
+      results.push(this.bfs(child + st));
+    }
+    const atomBoard = results.map((r) => r.result);
+    const atomResult = winner(atomBoard);
+    return {
+      type: "node",
+      result: atomResult,
+      board: results,
+    };
+  }
+  boardNumber(node: number): number | null {
+    const pos = this.pos(node);
+    if (pos.length > this.config.rank) {
+      throw new Error(`node is too large: ${node} -> ${pos}`);
+    }
+    if (pos.length === this.config.rank) {
+      return pos.reduce((a, x) => a * 9 + x);
+    }
+    return null;
+  }
+  pos(node: number): number[] {
+    const res = [];
+    while (node > 0) {
+      res.push((node - 1) % 9);
+      node = 0 | ((node - 1) / 9);
+    }
+    return res.reverse();
+  }
 }
