e80b5aa661802bf567739caed4fc2a1f334ed703
wass80 <wasss80@gmail.com>
2023-04-18 17:39:29 +0900

    fix code margin

--- elems/code.tsx
+++ elems/code.tsx
@@ -4,7 +4,11 @@ import { coldarkDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
 
 const Code = ({ children, lang }) => {
   return (
-    <SyntaxHighlighter language={lang} style={coldarkDark}>
+    <SyntaxHighlighter
+      language={lang}
+      style={coldarkDark}
+      customStyle={{ margin: "0 0 1rem 0" }}
+    >
       {children.trim()}
     </SyntaxHighlighter>
   );
