360dcf8ee61eb0ee30f434e00b553573089ff834
wass80 <wasss80@gmail.com>
2023-09-06 11:19:55 +0900

    use swapped math web font

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import "react-notion-x/src/styles.css";
 import "prismjs/themes/prism-tomorrow.css";
-import "katex/dist/katex.min.css";
+import "./katex.swap.css";
 import "./fix-notion.css";
 
 function MyApp({ Component, pageProps }) {
